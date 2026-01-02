2fe812428f9450265e525d1bb991291648818b69
Peter Muraya <sammypeter1944@gmail.com>
2026-01-02 23:17:20 +0300

    chore: remove pages/admin/login.tsx

--- pages/admin/login.tsx
+++
@@ -1,6 +0,0 @@
-import dynamic from 'next/dynamic';
-const AdminLogin = dynamic(() => import('../../src/pages/admin/AdminLogin'), { ssr: false });
-
-export default function Page() {
-  return <AdminLogin />;
-}
