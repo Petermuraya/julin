622a5da6761ea2e69cdaea90fb4c67d819aace9a
Peter Muraya <sammypeter1944@gmail.com>
2026-01-02 23:18:37 +0300

    chore: update global.d.ts

---
+++ global.d.ts
@@ -0,0 +1,30 @@
+declare module '*.svg' {
+  import { FC, SVGProps } from 'react';
+  const content: FC<SVGProps<SVGElement>>;
+  export default content;
+}
+
+declare module '*.png' {
+  const content: string;
+  export default content;
+}
+
+declare module '*.jpg' {
+  const content: string;
+  export default content;
+}
+
+declare module '*.jpeg' {
+  const content: string;
+  export default content;
+}
+
+declare module '*.gif' {
+  const content: string;
+  export default content;
+}
+
+declare module '*.webp' {
+  const content: string;
+  export default content;
+}
