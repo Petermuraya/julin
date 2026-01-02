b711581e325ad35be73e0d9b01ad1afe3b98cfae
Peter Muraya <sammypeter1944@gmail.com>
2026-01-02 23:17:19 +0300

    chore: remove pages/admin/index.tsx

--- pages/admin/index.tsx
+++
@@ -1,6 +0,0 @@
-import dynamic from 'next/dynamic';
-const AdminDashboard = dynamic(() => import('../../src/pages/admin/AdminDashboard'), { ssr: false });
-
-export default function Page() {
-  return <AdminDashboard />;
-}
