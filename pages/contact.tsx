6d1defef8d679bab3529d16095a2a187f39470d9
Peter Muraya <sammypeter1944@gmail.com>
2026-01-02 23:17:23 +0300

    chore: remove pages/contact.tsx

--- pages/contact.tsx
+++
@@ -1,6 +0,0 @@
-import dynamic from 'next/dynamic';
-const ContactPage = dynamic(() => import('../src/pages/Contact'), { ssr: false });
-
-export default function Page() {
-  return <ContactPage />;
-}
