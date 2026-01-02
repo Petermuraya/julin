f69d4b4f2766b2dbc252c7e58ac8f8dbad4ea6a4
Peter Muraya <sammypeter1944@gmail.com>
2026-01-02 23:17:22 +0300

    chore: remove pages/blog/[slug].tsx

--- pages/blog/[slug].tsx
+++
@@ -1,8 +0,0 @@
-import dynamic from 'next/dynamic';
-import { useRouter } from 'next/router';
-const BlogDetail = dynamic(() => import('../../src/pages/BlogDetailPage'), { ssr: false });
-
-export default function Page() {
-  const router = useRouter();
-  return <BlogDetail key={router.asPath} />;
-}
