8dd4eadd62e5d3e47d2578b0499d3528aa57917a
Peter Muraya <sammypeter1944@gmail.com>
2026-01-02 23:17:20 +0300

    chore: remove pages/admin/profile.tsx

--- pages/admin/profile.tsx
+++
@@ -1,6 +0,0 @@
-import dynamic from 'next/dynamic';
-const AdminProfile = dynamic(() => import('../../src/pages/admin/AdminProfile'), { ssr: false });
-
-export default function Page() {
-  return <AdminProfile />;
-}
