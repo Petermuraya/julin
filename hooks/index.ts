34cd7f3326b8f50b8b40316bc5f5efe947a57722
Peter Muraya <sammypeter1944@gmail.com>
2026-01-02 23:18:38 +0300

    chore: update hooks/

---
+++ hooks/index.ts
@@ -0,0 +1,4 @@
+export { default as useInView } from "@/hooks/use-in-view";
+export { default as useMediaQuery } from "@/hooks/use-media-query";
+export { default as useMobile } from "@/hooks/use-mobile";
+export { default as useToast } from "@/hooks/use-toast";
