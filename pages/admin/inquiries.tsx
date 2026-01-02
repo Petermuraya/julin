733832e6b19cc7fe67f278249d281ea471d06845
Peter Muraya <sammypeter1944@gmail.com>
2026-01-02 23:17:20 +0300

    chore: remove pages/admin/inquiries.tsx

--- pages/admin/inquiries.tsx
+++
@@ -1,6 +0,0 @@
-import dynamic from 'next/dynamic';
-const AdminInquiries = dynamic(() => import('../../src/pages/admin/AdminInquiries'), { ssr: false });
-
-export default function Page() {
-  return <AdminInquiries />;
-}
