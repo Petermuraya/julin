8f7ea5cf6da8e18d6582c246053ecd9ca293e0fb
Peter Muraya <sammypeter1944@gmail.com>
2026-01-02 23:17:22 +0300

    chore: remove pages/blogs.tsx

--- pages/blogs.tsx
+++
@@ -1,6 +0,0 @@
-import dynamic from 'next/dynamic';
-const BlogsPage = dynamic(() => import('../src/pages/BlogsPage'), { ssr: false });
-
-export default function Page() {
-  return <BlogsPage />;
-}
