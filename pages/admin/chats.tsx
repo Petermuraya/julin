1577a47df19c97e561231a85d3b4982d1b3638b5
Peter Muraya <sammypeter1944@gmail.com>
2026-01-02 23:17:19 +0300

    chore: remove pages/admin/chats.tsx

--- pages/admin/chats.tsx
+++
@@ -1,6 +0,0 @@
-import dynamic from 'next/dynamic';
-const AdminChatDashboard = dynamic(() => import('../../src/pages/admin/AdminChatDashboard'), { ssr: false });
-
-export default function Page() {
-  return <AdminChatDashboard />;
-}
