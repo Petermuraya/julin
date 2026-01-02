715da334004022400f070293e6c3675044815cbb
Peter Muraya <sammypeter1944@gmail.com>
2026-01-02 23:17:19 +0300

    chore: remove pages/admin/blogs.tsx

--- pages/admin/blogs.tsx
+++
@@ -1,6 +0,0 @@
-import dynamic from 'next/dynamic';
-const AdminBlogs = dynamic(() => import('../../src/pages/admin/AdminBlogs'), { ssr: false });
-
-export default function Page() {
-  return <AdminBlogs />;
-}
