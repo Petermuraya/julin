317feaefd45ce2a4426fce23f0197eb62daabf68
Peter Muraya <sammypeter1944@gmail.com>
2026-01-02 23:17:24 +0300

    chore: remove pages/properties.tsx

--- pages/properties.tsx
+++
@@ -1,6 +0,0 @@
-import dynamic from 'next/dynamic';
-const PropertiesPage = dynamic(() => import('../src/pages/PropertiesPage'), { ssr: false });
-
-export default function Page() {
-  return <PropertiesPage />;
-}
