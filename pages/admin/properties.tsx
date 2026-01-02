b610231d19e80b92ba4e465d753e94049814fa1f
Peter Muraya <sammypeter1944@gmail.com>
2026-01-02 23:17:21 +0300

    chore: remove pages/admin/properties.tsx

--- pages/admin/properties.tsx
+++
@@ -1,6 +0,0 @@
-import dynamic from 'next/dynamic';
-const AdminProperties = dynamic(() => import('../../src/pages/admin/AdminProperties'), { ssr: false });
-
-export default function Page() {
-  return <AdminProperties />;
-}
