02d463a8c53831c10b73635647783a4a08c6765c
Peter Muraya <sammypeter1944@gmail.com>
2026-01-02 23:17:21 +0300

    chore: remove pages/admin/submissions.tsx

--- pages/admin/submissions.tsx
+++
@@ -1,6 +0,0 @@
-import dynamic from 'next/dynamic';
-const AdminSubmissions = dynamic(() => import('../../src/pages/admin/AdminSubmissions'), { ssr: false });
-
-export default function Page() {
-  return <AdminSubmissions />;
-}
